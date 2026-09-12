d189bf9dc913aab3d4326e865943f8e464c2cc52
FelipeKenxi <felipemelo@estudante.ufscar.br>
2026-09-12 15:21:53 -0300

    Adição de struct para receber dados

--- kernel/virtio.c
+++ kernel/virtio.c
@@ -17,6 +17,7 @@
 #define VIRTIO_QUEUE_NUM_MAX        0x00D // 0x034 bytes
 #define VIRTIO_QUEUE_NUM            0x00E // 0x038 bytes
 #define VIRTIO_QUEUE_READY          0x011 // 0x044 bytes
+#define VIRTIO_QUEUE_NOTIFY         0x014 // 0x050 bytes
 #define VIRTIO_INTERRUPT_STATUS     0x018 // 0x060 bytes P.S: na documentação oficial esse valor esta como 0x60 por algum motivo
 #define VIRTIO_INTERRUPT_ACK        0x019 // 0x64 bytes
 #define VIRTIO_QUEUE_DESC_LO        0x020 // 0x080 bytes
@@ -64,20 +65,63 @@ struct virtq_used {
 } __attribute__((packed));
 
 
+#define VIRTQ_SIZE 128
+
+/* Estrutura para o kernel controlar o estado da fila */
+struct virtqueue_info {
+    struct virtq_desc *desc;
+    struct virtq_avail *avail;
+    struct virtq_used *used;
+    uint16_t last_used_idx; // Controla até onde o kernel já leu
+    uint16_t avail_idx;     // Controla onde colocar o próximo buffer livre
+};
+
+// Filas globais (0 = RX, 1 = TX)
+struct virtqueue_info virtqueues[2];
+
+
 /*
 Função interna para tratar a interrupção vinda da placa de rede.
 */
 void virtio_net_irq_handler(void) {
 
-    volatile uint32_t *mmio = (volatile uint32_t *) 0x0A000000;
+    volatile uint32_t *mmio = VIRTIO0_BASE;
 
     //Lê o registrador INTERRUPT_STATUS
-    if(mmio[VIRTIO_INTERRUPT_STATUS] == 1)
-        kputs(">>> PACOTE DE REDE RECEBIDO<<<\n");
-    
-    // Ler o anel USED da virqueue 
-    // TODO: processar_pacote_rx();
+    if(mmio[VIRTIO_INTERRUPT_STATUS] & 1){
+        struct virtqueue_info *rx = &virtqueues[0];
 
+        while(rx->last_used_idx != rx->used->idx){
+            uint16_t used_ring_index = rx->last_used_idx % VIRTQ_SIZE;
+
+            struct virtq_used_elem *elem = &rx->used->ring[used_ring_index];
+
+            uint32_t desc_id = elem->id;  
+            uint32_t pkt_len = elem->len;
+            
+            char *buffer_recebido = (char *)(uintptr_t) rx->desc[desc_id].addr;
+
+            kputs(">>> PACOTE DE REDE RECEBIDO<<<\n");
+
+            //O virtio usa os 12 primeiro bytes do buffer para um cabeçalho proprio antes do da ethernet
+            char *frame_ethernet = buffer_recebido + 12;
+
+            //TODO: processar pacote
+
+            uint16_t novo_avail_idx = rx->avail_idx % VIRTQ_SIZE;
+            rx->avail->ring[novo_avail_idx] = desc_id;
+            rx->avail_idx++;
+            
+            rx->last_used_idx++;
+            
+        }
+
+        rx->avail->idx = rx->avail_idx;
+        
+        // Notifica a fila RX (offset 0x050 = Queue Notify)
+        mmio[VIRTIO_QUEUE_NOTIFY] = 0;    
+    }
+    
     // Avisar a placa Virtio que a interrupção foi tratada escrevendo no registrador INTERRUPT_ACK.
     mmio[VIRTIO_INTERRUPT_ACK] = 1;
 }
@@ -94,13 +138,20 @@ void virtqueue_config(volatile uint32_t *mmio, int32_t num){
         // Fila nao suportada
         return;
     }
-    uint32_t queue_size = (queue_max > 128) ? 128 : queue_max;
+    uint32_t queue_size = VIRTQ_SIZE;
     mmio[VIRTIO_QUEUE_NUM] = queue_size;
 
     //Alocar as memorias da virtqueue
-    uintptr_t desc_addr = (uintptr_t) pmm_alloc_block();
-    uintptr_t avail_addr = (uintptr_t) pmm_alloc_block();
-    uintptr_t used_addr = (uintptr_t) pmm_alloc_block();
+
+    virtqueues[num].desc  = (struct virtq_desc *) pmm_alloc_block();
+    virtqueues[num].avail = (struct virtq_avail *) pmm_alloc_block();
+    virtqueues[num].used  = (struct virtq_used *) pmm_alloc_block();
+    virtqueues[num].last_used_idx = 0;
+    virtqueues[num].avail_idx = 0;
+
+    uintptr_t desc_addr = (uintptr_t) virtqueues[num].desc;
+    uintptr_t avail_addr = (uintptr_t) virtqueues[num].avail;
+    uintptr_t used_addr = (uintptr_t) virtqueues[num].used;
 
     // Informar os enderecos ao hardware (divididos em Low e High 32-bits) 
     mmio[VIRTIO_QUEUE_DESC_LO] = (uint32_t) desc_addr;
@@ -111,6 +162,25 @@ void virtqueue_config(volatile uint32_t *mmio, int32_t num){
 
     mmio[VIRTIO_QUEUE_DEV_LO]  = (uint32_t) used_addr;
     mmio[VIRTIO_QUEUE_DEV_HI]  = (uint32_t) (used_addr >> 32);
+
+
+    if (num == 0) { // Fila RX
+        for (int i = 0; i < queue_size; i++) {
+            // Aloca um buffer no heap do kernel
+            void *buffer = kmalloc(2048); 
+            
+            // Configura o descritor apontando para o buffer
+            virtqueues[num].desc[i].addr = (uint64_t)(uintptr_t)buffer;
+            virtqueues[num].desc[i].len = 2048;
+            virtqueues[num].desc[i].flags = 2; // VIRTQ_DESC_F_WRITE (Dispositivo pode escrever aqui)
+            virtqueues[num].desc[i].next = 0;
+            
+            // Coloca o ID do descritor no anel disponível
+            virtqueues[num].avail->ring[i] = i;
+        }
+        // Avisa a placa que disponibilizamos os índices de 0 a queue_size
+        virtqueues[num].avail->idx = queue_size;
+    }
 
     //Habilita a fila
     mmio[VIRTIO_QUEUE_READY] = 1;
